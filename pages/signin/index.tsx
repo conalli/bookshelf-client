1880214f103ae2cf268af455cc6ca1773dadad48
Conall <conallimlah@gmail.com>
2022-01-16 18:49:30 +0900

    add basic signin route

---
+++ pages/signin/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import SignInForm from "../../components/SignInForm";
+
+const SignIn = () => {
+  return (
+    <>
+      <SignInForm type="Log in" />
+    </>
+  );
+};
+
+export default SignIn;
