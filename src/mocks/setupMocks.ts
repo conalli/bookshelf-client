9f09b6dc1cb46ff536a491c60d67444ebc33d034
Conall <conallimlah@gmail.com>
2022-01-17 00:28:11 +0900

    refactor setup mocks

--- src/mocks/setupMocks.ts
+++ src/mocks/setupMocks.ts
@@ -4,8 +4,5 @@ export const setupMocks = async () => {
   if (BROWSER) {
     const { worker } = await import("./browser");
     worker.start();
-  } else {
-    const { server } = await import("./server");
-    server.listen();
   }
 };
