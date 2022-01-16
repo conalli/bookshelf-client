31912c87573a31971a00183d2f806525879b3618
Conall <conallimlah@gmail.com>
2022-01-16 23:26:24 +0900

    setup mocks for local dev

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,12 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "next-themes";
 
+if (process.env.NEXT_PUBLIC_API_MOCKING === "enabled") {
+  import("../src/mocks/setupMocks").then(({ setupMocks }) => {
+    setupMocks();
+  });
+}
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider attribute="class" enableSystem defaultTheme="system">
