fa9252ff5ef2479c343d959a2b3dc1952f2e9d3f
Conall <conallimlah@gmail.com>
2022-01-17 01:53:04 +0900

    add basic SignInForm tests

---
+++ src/components/SignInForm/SignInForm.test.tsx
@@ -0,0 +1,30 @@
+import { render, screen } from "@testing-library/react";
+import SignInForm from ".";
+
+describe("Bookshelf Logo", () => {
+  beforeEach(() => {
+    render(<SignInForm type="Log in" />);
+  });
+  describe("Should render form elements", () => {
+    test("name label", () => {
+      const nameLabel = screen.getByText(/name:/i);
+      expect(nameLabel).toBeInTheDocument();
+    });
+    test("name input", () => {
+      const nameInput = screen.getByRole("textbox");
+      expect(nameInput).toBeInTheDocument();
+    });
+    test("password label", () => {
+      const passwordLabel = screen.getByText(/password:/i);
+      expect(passwordLabel).toBeInTheDocument();
+    });
+    test("password input", () => {
+      const passwordInput = screen.getByPlaceholderText("password");
+      expect(passwordInput).toBeInTheDocument();
+    });
+    test("log in button", () => {
+      const button = screen.getByRole("button", { name: /log in/i });
+      expect(button).toBeInTheDocument();
+    });
+  });
+});
