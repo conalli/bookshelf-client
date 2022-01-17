78b03748f93ecd421c7b46db74cb50bc3fa535f8
Conall <conallimlah@gmail.com>
2022-01-17 23:32:51 +0900

    change set to addCmd

--- src/utils/APIEndpoints.ts
+++ src/utils/APIEndpoints.ts
@@ -1,7 +1,7 @@
 export const ReqURL = {
   base: process.env.NEXT_PUBLIC_API_BASE,
   getCmds: process.env.NEXT_PUBLIC_API_GETCMDS,
-  setCmd: process.env.NEXT_PUBLIC_API_SETCMD,
+  addCmd: process.env.NEXT_PUBLIC_API_SETCMD,
   delCmd: process.env.NEXT_PUBLIC_API_DELCMD,
   delAccount: process.env.NEXT_PUBLIC_API_DELACC,
 };
