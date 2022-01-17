2d38478cf8052af498600b1b102183179f2b18ac
Conall <conallimlah@gmail.com>
2022-01-17 23:31:34 +0900

    setup react-query and devtools

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,10 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { QueryClientProvider, QueryClient } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+import { Hydrate } from "react-query/hydration";
 import { ThemeProvider } from "next-themes";
+import { useState } from "react";
 
 if (process.env.NEXT_PUBLIC_API_MOCKING === "enabled") {
   import("../src/mocks/setupMocks").then(({ setupMocks }) => {
@@ -9,10 +13,16 @@ if (process.env.NEXT_PUBLIC_API_MOCKING === "enabled") {
 }
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const [queryClient] = useState(() => new QueryClient());
   return (
-    <ThemeProvider attribute="class" enableSystem defaultTheme="system">
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <ThemeProvider attribute="class" enableSystem defaultTheme="system">
+          <Component {...pageProps} />
+          <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
+        </ThemeProvider>
+      </Hydrate>
+    </QueryClientProvider>
   );
 }
 
