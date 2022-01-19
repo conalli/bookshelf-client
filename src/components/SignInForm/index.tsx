95adfba0c250664dbbbd8b42167eca739376414b
Conall <conallimlah@gmail.com>
2022-01-20 02:24:13 +0900

    update SignInForm

--- src/components/SignInForm/index.tsx
+++ src/components/SignInForm/index.tsx
@@ -1,4 +1,5 @@
 import { ErrorMessage, Field, Form, Formik, FormikHelpers } from "formik";
+import { motion } from "framer-motion";
 import React from "react";
 import { object, string } from "yup";
 import { useAuth } from "../../hooks/useAuth";
@@ -18,10 +19,12 @@ const SignInForm: React.FC<SignInFormProps> = ({ type }) => {
   const { isAuthLoading, logIn } = useAuth();
   const schema = object().shape({
     name: string()
-      .min(3, "Please enter a name with 3 or more characters")
+      .min(3, "Name: minimum 3 characters")
+      .max(12, "Name cannot be longer than 12 characters")
       .required("Please enter your username"),
     password: string()
-      .min(5, "Please enter a password with 5 or more characters")
+      .min(5, "Password: min 5 characters")
+      .max(20, "Password cannot be longer than 20 characters")
       .required("Please enter your password"),
   });
   return (
@@ -37,36 +40,51 @@ const SignInForm: React.FC<SignInFormProps> = ({ type }) => {
         }}
       >
         {({ isSubmitting }) => (
-          <Form className="flex flex-col items-start">
-            <label htmlFor="name">Name:</label>
-            <p className="text-bk-orange font-bold">
-              <ErrorMessage name="name" />
-            </p>
-            <Field
-              id="name"
-              name="name"
-              placeholder="name"
-              type="text"
-              disabled={isSubmitting || isAuthLoading}
-            />
-            <label htmlFor="password">Password:</label>
-            <p className="text-bk-orange font-bold">
-              <ErrorMessage name="password" />
-            </p>
-            <Field
-              id="password"
-              name="password"
-              placeholder="password"
-              type="password"
-              disabled={isSubmitting || isAuthLoading}
-            />
-            <button
-              type="submit"
-              disabled={isSubmitting || isAuthLoading}
-              className="bg-bk-blue px-3 py-1 rounded-sm text-gray-50"
-            >
-              {type}
-            </button>
+          <Form>
+            <div className="flex flex-col gap-2 py-2 text-xl">
+              <label htmlFor="name" className="pl-1">
+                Name:
+              </label>
+              <Field
+                id="name"
+                name="name"
+                placeholder="name"
+                type="text"
+                disabled={isSubmitting || isAuthLoading}
+                autoCorrect="off"
+                length={50}
+                className="appearance-none bg-gray-100  text-gray-900 text-sm rounded-lg focus:ring-bk-blue focus:border-bk-blue block w-full p-2.5 dark:bg-gray-700 dark:placeholder-gray-400 dark:text-white dark:focus:ring-bk-orange dark:focus:border-bk-orange shadow-md"
+              />
+              <p className="text-bk-blue dark:text-bk-orange font-semibold pl-1 text-md">
+                <ErrorMessage name="name" />
+              </p>
+            </div>
+            <div className="flex flex-col gap-2 py-2 text-xl">
+              <label htmlFor="password">Password:</label>
+              <Field
+                id="password"
+                name="password"
+                placeholder="password"
+                type="password"
+                disabled={isSubmitting || isAuthLoading}
+                autoCorrect="off"
+                length={50}
+                className="appearance-none bg-gray-100  text-gray-900 text-sm rounded-lg focus:ring-bk-blue focus:border-bk-blue block w-full p-2.5 dark:bg-gray-700 dark:placeholder-gray-400 dark:text-white dark:focus:ring-bk-orange dark:focus:border-bk-orange shadow-md"
+              />
+              <p className="text-bk-blue dark:text-bk-orange font-semibold pl-1 text-md">
+                <ErrorMessage name="password" />
+              </p>
+              <div className="flex justify-center py-10">
+                <motion.button
+                  whileHover={{ scale: 1.1 }}
+                  type="submit"
+                  disabled={isSubmitting || isAuthLoading}
+                  className="bg-bk-blue dark:bg-bk-orange text-xl px-5 py-2 w-40 hover:opacity-90 rounded shadow-md"
+                >
+                  {type}
+                </motion.button>
+              </div>
+            </div>
           </Form>
         )}
       </Formik>
