f086194343d535bd94c4536036350fba15439dd8
Conall <conallimlah@gmail.com>
2022-01-20 00:34:48 +0900

    add basic motion to nav

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -1,3 +1,4 @@
+import { motion } from "framer-motion";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import React from "react";
@@ -9,21 +10,24 @@ const Nav = () => {
   const navigateHome = () => router.push("/");
   return (
     <nav className="flex flex-row gap-2 justify-between py-2">
-      <button
+      <motion.button
+        whileHover={{ scale: 1.1 }}
         onClick={navigateHome}
         className="flex items-center p-2 mx-4 bg-white dark:bg-neutral-900 rounded-md shadow-lg"
       >
         <BookshelfLogo className={{ main: "h-12 w-28" }} />
-      </button>
+      </motion.button>
 
       <div className="flex flex-row gap-1 items-center mx-4 divide-x divide-bk-blue dark:divide-bk-orange  bg-white dark:bg-neutral-900 shadow-lg rounded-md">
         <Link href="/learn">
-          <a className="py-2 px-4 ml-1 hover:dark:text-orange-300 rounded-md">
+          <a className="py-2 px-4 ml-1 hover:text-bk-blue hover:dark:text-orange-300 rounded-md">
             Learn
           </a>
         </Link>
         <Link href="/dashboard">
-          <a className=" py-2 px-4 hover:dark:text-orange-300">Dashboard</a>
+          <a className=" py-2 px-4 hover:text-bk-blue hover:dark:text-orange-300">
+            Dashboard
+          </a>
         </Link>
         <div className="flex place-content-center px-4 mr-2">
           <ThemeToggleButton />
