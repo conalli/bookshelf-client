71e09dac4457f1e50f36704ed3615c1f5c7f2878
Conall <conallimlah@gmail.com>
2022-01-19 23:50:08 +0900

    update nav styling

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -9,19 +9,21 @@ const Nav = () => {
   const navigateHome = () => router.push("/");
   return (
     <nav className="flex flex-row gap-2 justify-between py-2">
-      <div className="flex items-center p-2 mx-4">
-        <button onClick={navigateHome}>
-          <BookshelfLogo className={{ main: "h-12 w-28" }} />
-        </button>
-      </div>
-      <div className="flex flex-row gap-1 items-center mx-4 divide-x divide-bk-blue dark:bg-slate-800 rounded-sm">
+      <button
+        onClick={navigateHome}
+        className="flex items-center p-2 mx-4 bg-white dark:bg-neutral-900 rounded-md shadow-lg"
+      >
+        <BookshelfLogo className={{ main: "h-12 w-28" }} />
+      </button>
+
+      <div className="flex flex-row gap-1 items-center mx-4 divide-x divide-bk-blue dark:divide-bk-orange  bg-white dark:bg-neutral-900 shadow-lg rounded-md">
         <Link href="/learn">
-          <a className="py-2 px-4 ml-1 hover:dark:bg-slate-700 rounded-md">
+          <a className="py-2 px-4 ml-1 hover:dark:text-orange-300 rounded-md">
             Learn
           </a>
         </Link>
         <Link href="/dashboard">
-          <a className=" py-2 px-4 hover:dark:bg-gray-700">Dashboard</a>
+          <a className=" py-2 px-4 hover:dark:text-orange-300">Dashboard</a>
         </Link>
         <div className="flex place-content-center px-4 mr-2">
           <ThemeToggleButton />
