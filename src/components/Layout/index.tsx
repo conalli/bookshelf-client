e4294e3394911cb149b108c03407091d20272e33
Conall <conallimlah@gmail.com>
2022-01-20 02:24:00 +0900

    update Layout styles

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -6,7 +6,7 @@ type LayoutProps = {
 };
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
-    <div className="grid grid-cols-[1fr_70%_1fr] grid-rows-[10vh_auto] bk-background max-w-screen">
+    <div className="grid grid-cols-[1fr_70%_1fr] grid-rows-[10vh_auto] bk-background min-h-screen max-w-screen">
       <header className="w-full col-start-2 row-start-1 m-auto">
         <Nav />
       </header>
