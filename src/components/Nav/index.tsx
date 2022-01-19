be7a62b396b29707d2b62ccf3a5c09f8a3fe78e3
Conall <conallimlah@gmail.com>
2022-01-20 02:23:29 +0900

    update nac

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -15,22 +15,28 @@ const Nav = () => {
         onClick={navigateHome}
         className="flex items-center p-2 mx-4 bg-white dark:bg-neutral-900 rounded-md shadow-lg"
       >
-        <BookshelfLogo className={{ main: "h-12 w-28" }} />
+        <BookshelfLogo className={{ main: "h-6 w-14 lg:h-12 lg:w-28" }} />
       </motion.button>
 
       <div className="flex flex-row gap-1 items-center mx-4 divide-x divide-bk-blue dark:divide-bk-orange  bg-white dark:bg-neutral-900 shadow-lg rounded-md">
-        <Link href="/learn">
-          <a className="py-2 px-4 ml-1 hover:text-bk-blue hover:dark:text-orange-300 rounded-md">
-            Learn
+        <Link href="/#about">
+          <a className="text-sm py-2 px-4 ml-1 hover:text-bk-blue hover:dark:text-orange-300 rounded-md">
+            About
           </a>
         </Link>
         <Link href="/dashboard">
-          <a className=" py-2 px-4 hover:text-bk-blue hover:dark:text-orange-300">
+          <a className="text-sm py-2 px-4 hover:text-bk-blue hover:dark:text-orange-300">
             Dashboard
           </a>
         </Link>
         <div className="flex place-content-center px-4 mr-2">
-          <ThemeToggleButton />
+          <ThemeToggleButton
+            buttonClass="inline-flex items-center justify-center w-8 h-8 lg:w-10 lg:h-10 focus:shadow-outline"
+            iconClass={{
+              dark: "p-2 text-orange-200 hover:text-orange-300 rounded-full",
+              light: "p-2 text-gray-500 hover:text-gray-800 rounded-full",
+            }}
+          />
         </div>
       </div>
     </nav>
