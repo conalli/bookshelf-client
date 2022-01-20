bceb2e83771124adb4ce9c0089229bd6fc19fcf3
Conall <conallimlah@gmail.com>
2022-01-20 17:07:21 +0900

    update styling for signin page

--- pages/signin/index.tsx
+++ pages/signin/index.tsx
@@ -9,19 +9,25 @@ const SignIn = () => {
   };
   const altType = formType === "Log in" ? "Sign up" : "Log in";
   return (
-    <div className="flex flex-col justify-center items-center bg-white dark:bg-neutral-900">
-      <h1 className="text-6xl py-10">{formType}</h1>
-      <SignInForm type={formType} />
-      <p className="text-xl py-10">
-        {formType === "Log in" ? "Already" : "Don't"} have an account?{" "}
-        <motion.button
-          whileHover={{ scale: 1.1, x: 10 }}
-          className="underline decoration-bk-blue dark:decoration-bk-orange decoration-2 px-1 rounded-sm"
-          onClick={changeFormType}
-        >
-          {altType}
-        </motion.button>
-      </p>
+    <div className="flex justify-center items-center md:items-start py-4 md:py-10 h-4/5 md:h-full">
+      <div className="grid grid-cols-6 grid-rows-4 bg-white dark:bg-neutral-900 shadow-md w-4/5 md:w-3/4 xl:w-2/4 h-4/5 rounded-md">
+        <h1 className="col-start-2 col-span-4 md:col-start-3 md:col-span-2 text-xl md:text-3xl lg:text-6xl py-2 md:py-10 self-start md:self-center justify-self-center">
+          {formType}
+        </h1>
+        <div className="col-start-2 col-span-4 md:col-start-3 md:col-span-2 row-start-2 row-span-2 self-center justify-self-center">
+          <SignInForm type={formType} />
+        </div>
+        <p className="col-start-1 col-span-6 py-4 md:col-start-2 md:col-span-4 self-end md:self-center justify-self-center row-start-4 text-xs md:text-xl lg:py-10">
+          {formType === "Log in" ? "Already" : "Don't"} have an account?{" "}
+          <motion.button
+            whileHover={{ scale: 1.1, x: 10 }}
+            className="underline decoration-bk-blue dark:decoration-bk-orange decoration-2 px-1 rounded-sm"
+            onClick={changeFormType}
+          >
+            {altType}
+          </motion.button>
+        </p>
+      </div>
     </div>
   );
 };
