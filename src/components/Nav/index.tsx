7486faaaffc81c5f3c816fd08d28d9b597d77cba
Conall <conallimlah@gmail.com>
2022-01-20 15:54:47 +0900

    adjust nav

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -13,23 +13,23 @@ const Nav = () => {
       <motion.button
         whileHover={{ scale: 1.1 }}
         onClick={navigateHome}
-        className="flex items-center p-2 mx-4 bg-white dark:bg-neutral-900 rounded-md shadow-lg"
+        className="flex items-center p-2 mr-4 bg-white dark:bg-neutral-900 rounded-md shadow-lg"
       >
         <BookshelfLogo className={{ main: "h-6 w-14 lg:h-12 lg:w-28" }} />
       </motion.button>
 
-      <div className="flex flex-row gap-1 items-center mx-4 divide-x divide-bk-blue dark:divide-bk-orange  bg-white dark:bg-neutral-900 shadow-lg rounded-md">
+      <div className="flex flex-row gap-1 items-center lg:mx-4 divide-x divide-bk-blue dark:divide-bk-orange  bg-white dark:bg-neutral-900 shadow-lg rounded-md">
         <Link href="/#about">
-          <a className="text-sm py-2 px-4 ml-1 hover:text-bk-blue hover:dark:text-orange-300 rounded-md">
+          <a className="text-xs md:text-sm py-2 px-2 md:px-4 ml-1 hover:text-bk-blue hover:dark:text-orange-300 rounded-md">
             About
           </a>
         </Link>
         <Link href="/dashboard">
-          <a className="text-sm py-2 px-4 hover:text-bk-blue hover:dark:text-orange-300">
+          <a className="text-xs md:text-sm py-2 px-2 md:px-4 hover:text-bk-blue hover:dark:text-orange-300">
             Dashboard
           </a>
         </Link>
-        <div className="flex place-content-center px-4 mr-2">
+        <div className="flex place-content-center px-2 md:px-4 md:mr-2">
           <ThemeToggleButton
             buttonClass="inline-flex items-center justify-center w-8 h-8 lg:w-10 lg:h-10 focus:shadow-outline"
             iconClass={{
