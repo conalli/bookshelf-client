802e8146052094394b04949bfd7bcdd2119e88f4
Conall <conallimlah@gmail.com>
2022-01-20 23:26:52 +0900

    refactor transitions

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,4 +1,4 @@
-import { motion, useAnimation } from "framer-motion";
+import { AnimatePresence, motion, useAnimation, Variants } from "framer-motion";
 import { useTheme } from "next-themes";
 import React, { ReactNode, useEffect } from "react";
 import Nav from "../Nav";
@@ -10,23 +10,31 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
   const { theme } = useTheme();
   const controls = useAnimation();
 
-  useEffect(() => {
-    controls.start({
+  const variants: Variants = {
+    themeTransition: {
       opacity: [0, 1],
       transition: { duration: 1.5 },
-    });
+    },
+  };
+
+  useEffect(() => {
+    controls.start("themeTransition");
   }, [controls, theme]);
 
   return (
-    <motion.div
-      animate={controls}
-      className="grid grid-cols-[1fr_90%_1fr] md:grid-cols-[1fr_70%_1fr] grid-rows-[10vh_auto] bk-background min-h-screen max-w-screen"
-    >
-      <header className="w-full col-start-2 row-start-1 m-auto">
-        <Nav />
-      </header>
-      <div className="col-start-2 row-start-2">{children}</div>
-    </motion.div>
+    <AnimatePresence>
+      <motion.div
+        id="top"
+        animate={controls}
+        variants={variants}
+        className="grid grid-cols-[1fr_90%_1fr] md:grid-cols-[1fr_70%_1fr] grid-rows-[10vh_auto] bk-background min-h-screen max-w-screen"
+      >
+        <header className="w-full col-start-2 row-start-1 m-auto">
+          <Nav />
+        </header>
+        <div className="col-start-2 row-start-2">{children}</div>
+      </motion.div>
+    </AnimatePresence>
   );
 };
 
