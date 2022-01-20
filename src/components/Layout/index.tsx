369c39b1df37d1055ab676d7d33dff1d6f25c2fa
Conall <conallimlah@gmail.com>
2022-01-20 15:55:24 +0900

    add transition on theme change

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,17 +1,32 @@
-import React, { ReactNode } from "react";
+import { motion, useAnimation } from "framer-motion";
+import { useTheme } from "next-themes";
+import React, { ReactNode, useEffect } from "react";
 import Nav from "../Nav";
 
 type LayoutProps = {
   children: ReactNode;
 };
 const Layout: React.FC<LayoutProps> = ({ children }) => {
+  const { theme } = useTheme();
+  const controls = useAnimation();
+
+  useEffect(() => {
+    controls.start({
+      opacity: [0, 1],
+      transition: { duration: 1.5 },
+    });
+  }, [controls, theme]);
+
   return (
-    <div className="grid grid-cols-[1fr_70%_1fr] grid-rows-[10vh_auto] bk-background min-h-screen max-w-screen">
+    <motion.div
+      animate={controls}
+      className="grid grid-cols-[1fr_70%_1fr] grid-rows-[10vh_auto] bk-background min-h-screen max-w-screen"
+    >
       <header className="w-full col-start-2 row-start-1 m-auto">
         <Nav />
       </header>
       <div className="col-start-2 row-start-2">{children}</div>
-    </div>
+    </motion.div>
   );
 };
 
