309e40368096fad91d83fd2cf2c536e34b333a7f
Conall <conallimlah@gmail.com>
2022-01-21 18:08:54 +0900

    close Modal on Cancel or Delete

--- src/components/Modal/DeleteCommandOverlay.tsx
+++ src/components/Modal/DeleteCommandOverlay.tsx
@@ -1,4 +1,5 @@
 import { AxiosResponse } from "axios";
+import { Dispatch, SetStateAction } from "react";
 import { UseMutationResult } from "react-query";
 import { Command } from "../../../pages/dashboard";
 import { User } from "../../hooks/useAuth";
@@ -9,12 +10,14 @@ type DeleteCommandOverlayProps = {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   del: UseMutationResult<AxiosResponse<any, any>, unknown, DelCmdData, unknown>;
   selected: Command | null;
+  setIsOpen: Dispatch<SetStateAction<boolean>>;
 };
 
 const DeleteCommandOverlay: React.FC<DeleteCommandOverlayProps> = ({
   user,
   del,
   selected,
+  setIsOpen,
 }) => {
   const deleteMessage = selected
     ? `Are you sure you want to delete command: ${selected.cmd} - ${selected.url}?`
@@ -23,7 +26,7 @@ const DeleteCommandOverlay: React.FC<DeleteCommandOverlayProps> = ({
     <div className="">
       <h1>Delete Command: </h1>
       <p>{deleteMessage}</p>
-      <button>Cancel</button>
+      <button onClick={() => setIsOpen(false)}>Cancel</button>
       {selected && (
         <button
           onClick={() => {
@@ -34,6 +37,7 @@ const DeleteCommandOverlay: React.FC<DeleteCommandOverlayProps> = ({
                 cmd: selected?.cmd,
               },
             });
+            if (del.isSuccess) setIsOpen(false);
           }}
         >
           Delete
