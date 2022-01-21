4c66abffe26de393ba05f099f4f9501d1eadb4af
Conall <conallimlah@gmail.com>
2022-01-22 00:34:56 +0900

    use status icon in table

--- src/components/CommandTable/index.tsx
+++ src/components/CommandTable/index.tsx
@@ -1,21 +1,27 @@
 import { TrashIcon } from "@heroicons/react/solid";
+import { motion } from "framer-motion";
 import React, { Dispatch, SetStateAction } from "react";
-import { Command } from "../../../pages/dashboard";
+import { Command, UpdateCommandStatus } from "../../../pages/dashboard";
 import { User } from "../../hooks/useAuth";
 import { useGetCmdData } from "../../hooks/useCmdData";
+import StatusIcon from "./StatusIcon";
 
 type CommandTableProps = {
   user: User | null;
   openModal: Dispatch<SetStateAction<boolean>>;
   setModalType: Dispatch<SetStateAction<"add" | "del" | undefined>>;
+  selected: Command | null;
   setSelected: Dispatch<SetStateAction<Command | null>>;
+  cmdStatus: UpdateCommandStatus;
 };
 
 const CommandTable: React.FC<CommandTableProps> = ({
   user,
   openModal,
   setModalType,
+  selected,
   setSelected,
+  cmdStatus,
 }) => {
   const { data } = useGetCmdData(
     user?.apiKey,
@@ -26,24 +32,47 @@ const CommandTable: React.FC<CommandTableProps> = ({
   const cmds = data?.data;
 
   return (
-    <table className="w-full mt-6 lg:w-2/4 m-auto bg-white dark:bg-neutral-800 rounded shadow">
+    <motion.table
+      initial={{ opacity: 0, x: 20 }}
+      animate={{
+        opacity: 1,
+        x: 0,
+        transition: {
+          duration: 0.6,
+        },
+      }}
+      exit={{ opacity: 0, x: -20 }}
+      className="w-full mt-6 lg:w-2/4 m-auto bg-white dark:bg-neutral-800 rounded shadow"
+    >
       <thead className="text-left text-white bg-bk-blue dark:bg-bk-orange rounded-lg">
-        <tr className="rounded-md">
-          <th className="border-r-bk-blue p-2">Command</th>
-          <th className="border-r-bk-blue p-2">URL</th>
-          <th className="text-center p-2">Del</th>
+        <tr>
+          <th className="text-xs md:text-smp-2">Command</th>
+          <th className="text-xs md:text-smp-2">URL</th>
+          <th className="text-xs md:text-smtext-center p-2">Status</th>
+          <th className="text-xs md:text-smtext-center p-2">Delete</th>
         </tr>
       </thead>
-      <tbody className="">
+      <tbody>
         {cmds &&
           Object.keys(cmds).map((key: string) => {
             return (
               <tr key={key}>
-                <td className="border-r-2 border-r-bk-blue p-2">{key}</td>
-                <td className="border-r-2 border-r-bk-blue p-2">
+                <td className="text-xs md:text-smborder-r-2 border-r-bk-blue p-2">
+                  {key}
+                </td>
+                <td className="text-xs md:text-smborder-r-2 border-r-bk-blue p-2">
                   <a href={cmds[key]}>{cmds[key]}</a>
                 </td>
-                <td className="">
+                <td className="text-xs md:text-smborder-r-2 border-r-bk-blue">
+                  <div className="flex justify-center items-center w-full h-full">
+                    <StatusIcon
+                      key={key}
+                      selected={selected}
+                      cmdStatus={cmdStatus}
+                    />
+                  </div>
+                </td>
+                <td className="text-xs md:text-sm">
                   <button
                     onClick={() => {
                       setModalType("del");
@@ -59,7 +88,7 @@ const CommandTable: React.FC<CommandTableProps> = ({
             );
           })}
       </tbody>
-    </table>
+    </motion.table>
   );
 };
 
