9e114c9a8684d76bb45799a876dce6d8f4d5076c
Conall <conallimlah@gmail.com>
2022-01-21 18:08:30 +0900

    add AddCommandOverlay for Modal

---
+++ src/components/Modal/AddCommandOverlay.tsx
@@ -0,0 +1,94 @@
+import { AxiosResponse } from "axios";
+import { ChangeEvent, Dispatch, SetStateAction, useState } from "react";
+import { UseMutationResult } from "react-query";
+import { User } from "../../hooks/useAuth";
+import { AddCmdData } from "../../hooks/useCmdData";
+
+type AddCommandOverlayProps = {
+  user: User;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  add: UseMutationResult<AxiosResponse<any, any>, unknown, AddCmdData, unknown>;
+  setIsOpen: Dispatch<SetStateAction<boolean>>;
+};
+
+const AddCommandOverlay: React.FC<AddCommandOverlayProps> = ({
+  user,
+  add,
+  setIsOpen,
+}) => {
+  const [cmd, setCmd] = useState<string>("");
+  const [url, setUrl] = useState<string>("");
+
+  const updateData = (
+    type: "cmd" | "url",
+    e: ChangeEvent<HTMLInputElement>
+  ) => {
+    if (type === "cmd") {
+      setCmd(e.target.value);
+      return;
+    }
+    setUrl(e.target.value);
+  };
+
+  return (
+    <div className="flex flex-col">
+      <h1 className="text-3xl">Add Command: </h1>
+      <div className="py-2 lg:py-6 text-md md:text-xl">
+        <div className="py-2">
+          <label htmlFor="command" className="py-2 pl-1">
+            Command:
+          </label>
+          <input
+            id="command"
+            className="appearance-none bg-gray-100  text-gray-900 text-sm rounded-lg focus:ring-bk-blue focus:border-bk-blue block w-full p-2.5 dark:bg-gray-700 dark:placeholder-gray-400 dark:text-white dark:focus:ring-bk-orange dark:focus:border-bk-orange shadow-md"
+            type="text"
+            name="command"
+            onChange={(e: ChangeEvent<HTMLInputElement>) =>
+              updateData("cmd", e)
+            }
+          />
+        </div>
+        <div className="py-2">
+          <label htmlFor="URL" className="py-2 pl-1">
+            URL:
+          </label>
+          <input
+            id="URL"
+            className="appearance-none bg-gray-100  text-gray-900 text-sm rounded-lg focus:ring-bk-blue focus:border-bk-blue block w-full p-2.5 dark:bg-gray-700 dark:placeholder-gray-400 dark:text-white dark:focus:ring-bk-orange dark:focus:border-bk-orange shadow-md"
+            type="text"
+            name="URL"
+            onChange={(e: ChangeEvent<HTMLInputElement>) =>
+              updateData("url", e)
+            }
+          />
+        </div>
+      </div>
+      <div className="flex gap-2 w-full py-2 lg:py-6 ">
+        <button
+          onClick={() => setIsOpen(false)}
+          className="bg-bk-blue dark:bg-bk-orange text-sm md:text-xl px-5 py-2 w-24 md:w-40 hover:opacity-90 rounded shadow-md"
+        >
+          Cancel
+        </button>
+        <button
+          onClick={() => {
+            add.mutate({
+              apiKey: user.apiKey,
+              body: {
+                id: user.id,
+                cmd,
+                url,
+              },
+            });
+            if (add.isSuccess) setIsOpen(false);
+          }}
+          className="bg-green-400 text-sm md:text-xl px-5 py-2 w-24 md:w-40 hover:opacity-90 rounded shadow-md"
+        >
+          Add
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default AddCommandOverlay;
