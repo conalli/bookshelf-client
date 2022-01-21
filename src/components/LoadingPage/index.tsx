39bbd774fb0c5a1f5f2153c1fb2b98edcde4c10a
Conall <conallimlah@gmail.com>
2022-01-22 02:12:47 +0900

    add animation to LoadingPage

--- src/components/LoadingPage/index.tsx
+++ src/components/LoadingPage/index.tsx
@@ -15,17 +15,70 @@ const LoadingPage = () => {
         exit={{ opacity: 0 }}
         className="h-screen w-screen flex items-center justify-center"
       >
-        <svg
+        <motion.svg
           width="192"
           height="192"
           viewBox="0 0 192 192"
           fill="none"
           xmlns="http://www.w3.org/2000/svg"
         >
-          <rect x="64" y="62" width="22" height="100" fill="#F2994A" />
-          <rect x="86" y="73" width="21" height="89" fill="#EB5757" />
-          <rect x="107" y="31" width="22" height="131" fill="#63B3ED" />
-        </svg>
+          <motion.rect
+            initial={{ y: 0 }}
+            animate={{
+              y: [-20, 0],
+              transition: {
+                type: "spring",
+                duration: 0.5,
+                repeat: Infinity,
+                repeatType: "mirror",
+                repeatDelay: 0.1,
+              },
+            }}
+            x="64"
+            y="62"
+            width="22"
+            height="100"
+            fill="#F2994A"
+          />
+          <motion.rect
+            initial={{ y: 0 }}
+            animate={{
+              y: [-20, 0],
+              transition: {
+                type: "spring",
+                delay: 0.2,
+                duration: 0.5,
+                repeat: Infinity,
+                repeatType: "mirror",
+                repeatDelay: 0.1,
+              },
+            }}
+            x="86"
+            y="73"
+            width="21"
+            height="89"
+            fill="#EB5757"
+          />
+          <motion.rect
+            initial={{ y: 0 }}
+            animate={{
+              y: [-20, 0],
+              transition: {
+                type: "spring",
+                delay: 0.4,
+                duration: 0.5,
+                repeat: Infinity,
+                repeatType: "mirror",
+                repeatDelay: 0.1,
+              },
+            }}
+            x="107"
+            y="31"
+            width="22"
+            height="131"
+            fill="#63B3ED"
+          />
+        </motion.svg>
       </motion.div>
     </AnimatePresence>
   );
