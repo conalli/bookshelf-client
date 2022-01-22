60c707f7b59864c0beb3ff6890e19797c71ae565
Conall <conallimlah@gmail.com>
2022-01-23 00:58:34 +0900

    refactor StatusIcon logic

--- src/components/CommandTable/StatusIcon.tsx
+++ src/components/CommandTable/StatusIcon.tsx
@@ -3,6 +3,7 @@ import {
   CheckCircleIcon,
   ExclamationCircleIcon,
 } from "@heroicons/react/outline";
+import { useEffect, useState } from "react";
 import { Command, UpdateCommandStatus } from "../../../pages/dashboard";
 import Spinner from "../../Spinner";
 
@@ -17,28 +18,34 @@ const StatusIcon: React.FC<StatusIconProps> = ({
   cmd,
   selected,
 }) => {
-  if (
-    selected &&
-    selected.cmd === cmd &&
-    (cmdStatus.add.success || cmdStatus.del.success)
-  ) {
+  const { add, del } = cmdStatus;
+  const [iconType, setIconType] = useState<
+    "success" | "loading" | "error" | "default"
+  >("loading");
+  useEffect(() => {
+    if (selected && selected.cmd === cmd) {
+      if (add.success || del.success) setIconType("success");
+      if (add.loading || del.loading) setIconType("loading");
+      if (add.error || del.error) setIconType("error");
+    } else {
+      setIconType("default");
+    }
+  }, [
+    add.error,
+    add.loading,
+    add.success,
+    cmd,
+    del.error,
+    del.loading,
+    del.success,
+    selected,
+  ]);
+
+  if (iconType === "success")
     return <CheckCircleIcon className="text-green-500 h-4 w-4 md:h-6 md:w-6" />;
-  }
-  if (
-    selected &&
-    selected.cmd === cmd &&
-    (cmdStatus.add.loading || cmdStatus.del.loading)
-  ) {
-    return <Spinner />;
-  }
-  if (
-    selected &&
-    selected.cmd === cmd &&
-    (cmdStatus.add.error || cmdStatus.del.error)
-  ) {
-    return (
-      <ExclamationCircleIcon className="text-bk-red h-4 w-4  md:h-6 md:w-6" />
-    );
+  if (iconType === "loading") return <Spinner />;
+  if (iconType === "error") {
+    <ExclamationCircleIcon className="text-bk-red h-4 w-4  md:h-6 md:w-6" />;
   }
   return <CheckIcon className="text-bk-blue h-4 w-4 md:h-6 md:w-6" />;
 };
