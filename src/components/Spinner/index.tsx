8bf574c0aeb9d51141da47e6f0ae9ed43dddf843
Conall <conallimlah@gmail.com>
2022-01-23 21:38:54 +0900

    remove padding from spinner

--- src/components/Spinner/index.tsx
+++ src/components/Spinner/index.tsx
@@ -1,7 +1,7 @@
 const Spinner = () => {
   return (
     <svg
-      className="animate-spin -ml-1 mr-3 h-4 w-4"
+      className="animate-spin h-4 w-4"
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
       viewBox="0 0 24 24"
