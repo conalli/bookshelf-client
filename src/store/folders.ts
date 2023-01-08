351e217954f3474093240c29eb7386ad96cbf99a
Conall <conallimlah@gmail.com>
2023-01-08 22:39:25 +0900

    add store for folder state

---
+++ src/store/folders.ts
@@ -0,0 +1,39 @@
+import { atom } from "jotai";
+import { Folder } from "../utils/APITypes";
+
+export type FolderOpenState = { [name: string]: boolean };
+
+export const foldersAtom = atom<Folder>({} as Folder);
+export const openFoldersAtom = atom<FolderOpenState>({} as FolderOpenState);
+
+const findAllFolders = (
+  base: Folder,
+  openFolders: FolderOpenState
+): FolderOpenState => {
+  if (!base.folders) return openFolders;
+  const currFolders = base.folders.reduce((prev, curr) => {
+    prev[curr.name] = true;
+    return prev;
+  }, {} as FolderOpenState);
+  for (const f of base.folders) {
+    openFolders = { ...openFolders, ...findAllFolders(f, openFolders) };
+  }
+  return { ...openFolders, ...currFolders };
+};
+
+export const addOpenFoldersAtom = atom(null, (get, set) => {
+  const folder = get(foldersAtom);
+  if (!folder.folders) return;
+  const openFolders = findAllFolders(folder, {});
+  set(openFoldersAtom, openFolders);
+});
+
+export const updateOpenFoldersAtom = atom(
+  null,
+  (get, set, name: keyof FolderOpenState) => {
+    const openFolders = get(openFoldersAtom);
+    const curr = openFolders[name];
+    const currentlyOpenFolders = { ...openFolders, [name]: !curr };
+    set(openFoldersAtom, currentlyOpenFolders);
+  }
+);
