7f8424e7a736b3638b161401f04c4e440dcbadbc
Conall <conallimlah@gmail.com>
2023-01-10 15:07:37 +0900

    add jest-environment-jsdom

--- jest.config.ts
+++ jest.config.ts
@@ -9,7 +9,7 @@ const customJestConfig = {
   // if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
   setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
   moduleDirectories: ["node_modules", "<rootDir>/"],
-  testEnvironment: "jest-environment-jsdom",
+  testEnvironment: "jsdom",
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
