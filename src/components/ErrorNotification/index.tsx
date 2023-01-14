cc14710b6b732d512ba0c973c2fed20f8bb8acd9
Conall <conallimlah@gmail.com>
2023-01-14 23:04:31 +0900

    update error notification

--- src/components/ErrorNotification/index.tsx
+++ src/components/ErrorNotification/index.tsx
@@ -18,7 +18,7 @@ const ErrorNotification: React.FC<ErrorNotificationProps> = ({
       initial={{ opacity: 0, y: 50, scale: 0.3 }}
       animate={{ opacity: 1, y: 0, scale: 1 }}
       exit={{ opacity: 0, scale: 0.5, transition: { duration: 0.2 } }}
-      className="appearance-none w-72 h-24 m-3 bg-red-500 dark:bg-red-500 rounded shadow-md border-4 border-bk-red"
+      className="appearance-none w-72 h-24 m-3 bg-red-500 dark:bg-red-500 rounded shadow-md border-4 border-bk-red hover:cursor-pointer"
       onClick={() => closeErrorMessage(id)}
     >
       <div className="h-full w-full grid grid-cols-1 grid-rows-[1fr_4fr] p-1">
