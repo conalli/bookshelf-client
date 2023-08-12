8aff0c29d74935c94089d34020a6a4ab16684a4f
Conall <conallimlah@gmail.com>
2023-08-12 18:10:38 +0900

    update extension configs

---
+++ tailwind-workspace-preset.js
@@ -0,0 +1,20 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { fontFamily } = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  darkMode: "class",
+  theme: {
+    fontFamily: {
+      sans: ["var(--font-overpass)", "var(--font-murecho)", ...fontFamily.sans],
+    },
+    extend: {
+      colors: {
+        "bk-primary-light": "#ffffff01",
+        "bk-primary-dark": "#21212180",
+        "bk-orange": "#F2994A",
+        "bk-blue": "#63B3ED",
+        "bk-red": "#EB5757",
+      },
+    },
+  },
+};
